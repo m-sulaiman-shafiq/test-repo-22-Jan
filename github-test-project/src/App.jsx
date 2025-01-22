8625e3f239a6d69c9f13d1140c7b3ac2c9646070
m-sulaiman-shafique <muhammadsuleman761@gmail.com>
2025-01-22 15:55:40 +0500

    changes in reviewpage

--- github-test-project/src/App.jsx
+++ github-test-project/src/App.jsx
@@ -14,6 +14,14 @@ function App() {
       <h2>another change in reviewits</h2>
     <p>final changes</p>
     <h3>changes in research page</h3>
+
+      <div className='research'>
+         <p>RS-The "Research Page" is a hub for exploring and analyzing information, 
+          enabling users to access valuable insights and data. It is designed to facilitate 
+          in-depth research with organized resources, tools, and visuals that help users make
+           informed decisions. Whether for academic, business, or personal purposes, the page 
+           serves as a streamlined platform for knowledge discovery and innovation.</p>
+      </div>
     </>
   )
 }
